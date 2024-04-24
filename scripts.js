5752d748d0496762e4d6c16a458ce19a524af9ba
Brian Strickland <bstrick@gmail.com>
2024-04-24 16:34:46 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
