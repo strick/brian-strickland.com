f88640addcbb41a2169e499c7fad8eb7802672d5
Brian Strickland <bstrick@gmail.com>
2025-06-04 18:39:58 -0400

    add logging

--- api/post-comment/index.js
+++ api/post-comment/index.js
@@ -1,7 +1,10 @@
 const sql = require('mssql');
 
 module.exports = async function (context, req) {
+    context.log("Function triggered");
+
     const { name, comment, post_slug } = req.body;
+    context.log("Payload received:", { name, comment, post_slug });
 
     if (!name || !comment || !post_slug) {
         context.res = {
@@ -13,6 +16,8 @@ module.exports = async function (context, req) {
 
     try {
         await sql.connect(process.env.SQL_CONN_STRING);
+        context.log("Connected to SQL");
+
         await sql.query`
             INSERT INTO Comments (name, comment, post_slug)
             VALUES (${name}, ${comment}, ${post_slug})
@@ -23,6 +28,7 @@ module.exports = async function (context, req) {
             body: "Comment added"
         };
     } catch (err) {
+        context.log("DB error:", err.message);  // ✅ this is important
         context.res = {
             status: 500,
             body: "DB error: " + err.message
