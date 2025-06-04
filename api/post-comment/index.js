9b89f465c34b345a26c89472ba780ea720725bcd
Brian Strickland <bstrick@gmail.com>
2025-06-04 19:03:11 -0400

    fix attmpet

--- api/post-comment/index.js
+++ api/post-comment/index.js
@@ -1,11 +1,11 @@
 const sql = require('mssql');
 
 module.exports = async function (context, req) {
-    context.log("POST /api/post-comment triggered");
+    context.log('--- post-comment triggered ---');
 
     try {
         const { name, comment, post_slug } = req.body || {};
-        context.log("Received body:", req.body);
+        context.log('Received body:', { name, comment, post_slug });
 
         if (!name || !comment || !post_slug) {
             context.res = {
@@ -15,24 +15,24 @@ module.exports = async function (context, req) {
             return;
         }
 
-        context.log("Connecting to SQL...");
         await sql.connect(process.env.SQL_CONN_STRING);
-        context.log("Connected to SQL");
+        context.log('SQL connection established');
 
         await sql.query`
             INSERT INTO Comments (name, comment, post_slug)
             VALUES (${name}, ${comment}, ${post_slug})
         `;
+        context.log('Comment inserted');
 
         context.res = {
             status: 200,
             body: "Comment added"
         };
     } catch (err) {
-        context.log.error("ERROR:", err);
+        context.log.error('Error occurred:', err); // LOG TO DEPLOYED APP
         context.res = {
             status: 500,
-        body: `Function error: ${err.message || err.toString()}`
+            body: `Function error: ${err.message || err.toString()}`
         };
     }
 };
